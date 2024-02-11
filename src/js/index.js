1d98682a0fd905ce8f96ad31cad53df623a507f9
Stephane Candelas <stephanecandelas@gmail.com>
2024-02-11 17:43:30 +0000

    clean js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,2 @@
-import _ from 'lodash'
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
-
-console.log(_.join(['Index', 'module', 'loaded'], ' '))
-
-const toto = [1, 2, 3].map((n) => n * 2)
-toto.forEach((n) => console.log('n: ', n))
