fe765c67f0691fa17a4ff4d7bd64f743abdbaefa
Stephane Candelas <stephanecandelas@gmail.com>
2024-02-13 23:51:28 +0000

    add carousel 1 - travel suggestion

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,36 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    let carouselWidth = document.querySelector('.carousel-inner').scrollWidth
+    let cardWidth = document.querySelector('.carousel-item').clientWidth
+    let scrollPosition = 0
+
+    const next = document.querySelector('.carousel-control-next')
+    next.addEventListener('click', (e) => {
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth
+
+            const inner = document.querySelector('.carousel-inner')
+            inner.scrollTo({
+                left: scrollPosition,
+                top: 0,
+                behavior: 'smooth',
+            })
+        }
+    })
+
+    const prev = document.querySelector('.carousel-control-prev')
+    prev.addEventListener('click', (e) => {
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth
+
+            const inner = document.querySelector('.carousel-inner')
+            inner.scrollTo({
+                left: scrollPosition,
+                top: 0,
+                behavior: 'smooth',
+            })
+        }
+    })
+})
