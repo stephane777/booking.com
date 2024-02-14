e9400f94883cc0f69db638daa85f3e7cfdbbefad
Stephane Candelas <stephanecandelas@gmail.com>
2024-02-14 23:53:45 +0000

    add trending destination & fix carousel 2 on small screen

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,40 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 document.addEventListener('DOMContentLoaded', (e) => {
-    let carouselWidth = document.querySelector('.carousel-inner').scrollWidth
-    let cardWidth = document.querySelector('.carousel-item').clientWidth
+    ;['#carousel_1', '#carousel_2'].forEach((id) => {
+        carousel(id)
+    })
+})
+
+function carousel(id) {
+    // const isCarousel2 = id === '#carousel_2' ? 8 : 0
+    const padding = id === '#carousel_2' ? 8 : id === '#carousel_1' ? 0 : 0
+    const visibleCard = id === '#carousel_2' ? 4 : id === '#carousel_1' ? 2 : 2
+    let carouselWidth = document.querySelector(
+        id + ' .carousel-inner'
+    ).scrollWidth
+    let cardWidth =
+        document.querySelector(id + ' .carousel-item').clientWidth + padding
     let scrollPosition = 0
 
-    const next = document.querySelector('.carousel-control-next')
-    next.addEventListener('click', (e) => {
-        if (scrollPosition < carouselWidth - cardWidth * 4) {
-            scrollPosition += cardWidth
+    const next = document.querySelector(id + ' .carousel-control-next')
+    const prev = document.querySelector(id + ' .carousel-control-prev')
 
-            const inner = document.querySelector('.carousel-inner')
+    next.addEventListener('click', (e) => {
+        // console.group('Next')
+        // console.log('carouselWidth: ', carouselWidth)
+        // console.log('cardWidth: ', cardWidth)
+        // console.log('scrollPosition: ', scrollPosition)
+        // console.log(
+        //     'scrollPosition < carouselWidth - cardWidth * 4: ',
+        //     scrollPosition < carouselWidth - cardWidth * visibleCard
+        // )
+
+        // console.groupEnd()
+        if (scrollPosition < carouselWidth - cardWidth * visibleCard) {
+            scrollPosition += cardWidth
+            // console.log('next scrollPosition: ', scrollPosition)
+            const inner = document.querySelector(id + ' .carousel-inner')
             inner.scrollTo({
                 left: scrollPosition,
                 top: 0,
@@ -20,12 +44,11 @@ document.addEventListener('DOMContentLoaded', (e) => {
         }
     })
 
-    const prev = document.querySelector('.carousel-control-prev')
     prev.addEventListener('click', (e) => {
         if (scrollPosition > 0) {
             scrollPosition -= cardWidth
-
-            const inner = document.querySelector('.carousel-inner')
+            // console.log('prev scrollPosition: ', scrollPosition)
+            const inner = document.querySelector(id + ' .carousel-inner')
             inner.scrollTo({
                 left: scrollPosition,
                 top: 0,
@@ -33,4 +56,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
             })
         }
     })
-})
+}
